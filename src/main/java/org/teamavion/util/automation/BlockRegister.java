be8cdd5090bda8b6f1e36b8e31945293779dcd4c
Gabriel Tofvesson <gabriel.tofvesson@hotmail.com>
2017-05-12 01:58:00 +0200

    Deprecated obsolete classes

--- src/main/java/org/teamavion/util/automation/BlockRegister.java
+++ src/main/java/org/teamavion/util/automation/BlockRegister.java
@@ -1,6 +1,7 @@
 package org.teamavion.util.automation;
 
 @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Deprecated
 public @interface BlockRegister {
     Class<? extends net.minecraft.block.Block> value() default net.minecraft.block.Block.class;
     String name() default "";
--- src/main/java/org/teamavion/util/automation/ItemRegister.java
+++ src/main/java/org/teamavion/util/automation/ItemRegister.java
@@ -1,6 +1,7 @@
 package org.teamavion.util.automation;
 
 @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Deprecated
 public @interface ItemRegister {
     Class<? extends net.minecraft.item.Item> value() default net.minecraft.item.Item.class;
     String name() default "";
